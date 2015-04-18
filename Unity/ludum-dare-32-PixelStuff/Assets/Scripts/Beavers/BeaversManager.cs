f26bd94fd7b7496624bee15a9d8e4a60a789bd7d
Mathias Musset <mussetmathias@gmail.com>
2015-04-18 17:39:58 +0200

    modif minime BeaversManager

--- Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Beavers/BeaversManager.cs
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Beavers/BeaversManager.cs
@@ -11,16 +11,22 @@ public class BeaversManager : MonoBehaviour {
 	private GameObject m_beaverContainer;
 	private List<Beaver> m_listBeavers;
 
-	[SerializeField]
-	private BrothersManager m_brothersManager;
+	//[SerializeField]
+	//private  m_brothersManager;
 
 	// Use this for initialization
 	void Start () {
+		m_listBeavers = new List<Beaver> ();
 		m_beaverContainer.transform.position = GameObject.FindGameObjectWithTag ("SpawnArea").transform.position;
 
 
 		GameObject plop = Instantiate (m_beaverPrefab);
 		plop.transform.SetParent (m_beaverContainer.transform);
+		plop.transform.localPosition = Vector3.zero;
+
+		m_listBeavers.Add (plop.gameObject.GetComponent<Beaver> ());
+
+		Debug.Log (" list : " + m_listBeavers [0].transform.position);
 	}
 	
 	// Update is called once per frame
