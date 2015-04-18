d6602a639d47ade844aee413e622a8eb58f8d17a
Jeremy Morel <jeremy.morel.jm@gmail.com>
2015-04-18 17:15:49 +0200

    tree Start

---
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Tree/treeManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class treeManager : MonoBehaviour {
+
+	private GameObject currentTree;
+
+	private GameObject currentWeapon;
+	private Transform spawnArea;
+
+	public GameObject treePrefab;
+	public GameObject weaponTreePrefab;
+	
+	// Use this for initialization
+	void Start () {
+		currentTree = null;
+		spawnArea = GameObject.FindGameObjectWithTag ("SpawnArea").transform;
+	}
+
+	bool spawnTree ()
+	{
+		return currentTree ? false : true;
+	}
+
+	void refreshCurrent ()
+	{
+		if(currentTree.transform.position.x < -spawnArea.transform.position.x){
+			Destroy(currentTree);
+			currentTree = null;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnTree ()) {
+		currentTree = Instantiate(treePrefab);
+			// TODO Set the ancre transform
+			currentTree.transform.position = spawnArea.position;
+			currentTree.transform.SetParent (this.transform);
+		}
+		refreshCurrent ();
+	}
+}
