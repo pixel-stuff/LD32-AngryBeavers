cf2e5b92a3defcf5be074132af72652e679b3b6d
Mathias Musset <mussetmathias@gmail.com>
2015-04-18 12:50:50 +0200

    Script IA

---
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Beavers/Beaver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beaver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
