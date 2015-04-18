651082a4b05446aa12576d5c7431ef00c84055a5
pplans <pierre.plans@gmail.com>
2015-04-18 19:40:43 +0200

    Parallax slider
    v2

--- Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Parallax/ParallaxSlider.cs
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Parallax/ParallaxSlider.cs
@@ -4,24 +4,23 @@ using System.Collections;
 public class ParallaxSlider : MonoBehaviour {
 
 	public GameObject prefabBackground;
+	public float speedX = -0.1f;
 
+	private Vector3 speed;
 	private ArrayList backgroundList;
+	private Vector3 size;
 	// Use this for initialization
 	void Start () {
 		backgroundList = new ArrayList();
-		GameObject instance = null;
-		instance = (GameObject)Instantiate (prefabBackground);
-		backgroundList.Add (instance);
-		instance = (GameObject)Instantiate (prefabBackground);
-		backgroundList.Add (instance);
-		instance = (GameObject)Instantiate (prefabBackground);
-		backgroundList.Add (instance);
+		speed.x = speedX;
+		backgroundList.Add ((GameObject)Instantiate (prefabBackground));
+		backgroundList.Add ((GameObject)Instantiate (prefabBackground));
+		backgroundList.Add ((GameObject)Instantiate (prefabBackground));
 
 		foreach (GameObject bg in backgroundList) {
 			bg.transform.SetParent(gameObject.transform);
 		}
-		Vector3 size = ((GameObject)backgroundList[0]).GetComponent<Renderer>().bounds.size;
-		Debug.Log (size);
+		size = ((GameObject)backgroundList[0]).GetComponent<Renderer>().bounds.size;
 		((GameObject)backgroundList[0]).transform.Translate(new Vector3(-size.x, 0.0f, 0.0f));
 		((GameObject)backgroundList[1]).transform.Translate(new Vector3(0, 0.0f, 0.0f));
 		((GameObject)backgroundList[2]).transform.Translate(new Vector3(size.x, 0.0f, 0.0f));
@@ -29,6 +28,14 @@ public class ParallaxSlider : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		int i = 0;
+		foreach(GameObject slide in backgroundList) {
+			slide.transform.Translate(speed);
+			if(slide.transform.position.x<=-1.5f*size.x) {
+				print ("booyah "+i+" "+slide.ToString()+" "+slide.transform.position);
+				slide.transform.Translate(new Vector3(2.5f*size.x, 0.0f, 0.0f));
+			}
+			i++;
+		}
 	}
 }
