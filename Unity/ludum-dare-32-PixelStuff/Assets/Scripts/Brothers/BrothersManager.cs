27c8572da54be4c71268ed637284925badc0e4d4
David Miotti <david.miotti@wopata.com>
2015-04-18 15:31:18 +0200

    brothers: define states

--- Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Brothers/BrothersManager.cs
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Brothers/BrothersManager.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum BrothersState {
+	Idle,
+	MovingIntoScene,
+	CuttingLumber,
+	GrabbingTrunk,
+	PrepareAttack,
+	Attack,
+	Dying,
+	Died
+}
+
 public class BrothersManager : MonoBehaviour {
+	
+	public BrothersState state = BrothersState.Idle;
+
+	private Brother brother1;
+	private Brother brother2;
 
 	// Use this for initialization
 	void Start () {
-	
+		state = BrothersState.MovingIntoScene;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
