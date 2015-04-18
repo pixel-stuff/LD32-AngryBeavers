e7dc89f3f58140fec1f91de0261ad385c8152e71
Mathias Musset <mussetmathias@gmail.com>
2015-04-18 15:43:55 +0200

    Modif BeaversManager

--- Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Beavers/BeaversManager.cs
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/Beavers/BeaversManager.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BeaversManager : MonoBehaviour {
 
 	[SerializeField]
 	private GameObject m_beaverPrefab;
 
+	[SerializeField]
+	private GameObject m_beaverContainer;
+	private List<Beaver> m_listBeavers;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject plop = Instantiate (m_beaverPrefab);
+		plop.transform.SetParent (m_beaverContainer.transform);
 	}
 	
 	// Update is called once per frame
