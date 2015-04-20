9734e01bcfb72f371c039d7fcf0b62fecc87ae5a
Mathias Musset <mussetmathias@gmail.com>
2015-04-21 00:22:04 +0200

    ScoreGUI

---
+++ Unity/ludum-dare-32-PixelStuff/Assets/Scripts/ScoreGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnGUI(){
+		
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2, 100, 20), "Hello World!");
+	}
+}
